5abafed9d8ad3e8265437ef704877ab96d768068
Marc Dave <davevalencia17@gmail.com>
2024-05-15 09:42:26 +0800

    Implement Data Annotations for Validation

--- StockMarket with Social Media Platform/Dtos/Comment/UpdateCommentRequestDto.cs
+++ StockMarket with Social Media Platform/Dtos/Comment/UpdateCommentRequestDto.cs
@@ -1,8 +1,17 @@
-﻿namespace StockMarket_with_Social_Media_Platform.Dtos.Comment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockMarket_with_Social_Media_Platform.Dtos.Comment
 {
     public class UpdateCommentRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be 5 characters long")]
+        [MaxLength(280, ErrorMessage = "Title cannot be over 280 characters")]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be 5 characters long")]
+        [MaxLength(280, ErrorMessage = "Content cannot be over 280 characters")]
         public string Content { get; set; } = string.Empty;
     }
 }
--- StockMarket with Social Media Platform/Dtos/Stock/CreateStockRequestDto.cs
+++ StockMarket with Social Media Platform/Dtos/Stock/CreateStockRequestDto.cs
@@ -1,19 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StockMarket_with_Social_Media_Platform.Dtos.Stock
 {
     public class CreateStockRequestDto
     {
-
+        [Required]
+        [MaxLength(10, ErrorMessage = "Symbol cannot be over 10 characters")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "Symbol cannot be over 10 characters")]
         public string CompanyName { get; set; } = string.Empty;
 
+        [Required]
+        [Range(1, 1000000000)]
         [Column(TypeName = "decimal(18, 2")]
         public decimal Purchase { get; set; }
 
+        [Required]
+        [Range(0.001, 100)]
         [Column(TypeName = "decimal(18, 2")]
         public decimal LastDiv { get; set; }
+
+        [Required]
+        [MaxLength (10, ErrorMessage = "Industry cannot be over 10 characters")]
         public string Industry { get; set; } = string.Empty;
+
+        [Range(1, 5000000000)]
         public long MarketCap { get; set; }
     }
 }
